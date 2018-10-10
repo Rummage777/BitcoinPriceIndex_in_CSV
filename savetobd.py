5c733a1540b9e155cf4578f12323939c115be0ef
Rummage777 <777.free.writer@gmail.com>
2018-10-10 07:39:52 +0300

    Refactoring code

--- savetobd.py
+++ savetobd.py
@@ -1,13 +1,34 @@
-import sqlalchemy
-import
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+import mysqldb
+
+Base = declarative_base()
+
+
+class BpiData(Base):
+    __tablename__ = 'dateprice'
+    id = Column(Integer, primary_key=True)
+    date = Column(String)
+    price = Column(String)
+
+    def __init__(self, date, price):
+        self.date = date
+        self.price = price
+
+    def __repr__(self):
+        return "<BpiData('%s','%s')>" % (self.date, self.price)
+
+
+# Create table
+Base.metadata.create_all(engine)
 
 def write_bpidata_to_bd(data):
-    connection = dbapi.connect(user="root", dbname="bpibase", host="localhost")
+    connection = mysqldb.connect(user="root", dbname="bpibase", host="localhost")
     cursor = connection.cursor()
-    for date, price in date.items():
-
-
+    for date, price in data.items():
+        record = BpiData(date, price)
+        session.add(record)
     cursor.close()
     conection.commit()
     connection.close()
-
